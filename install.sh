be17edaf05f4275230892f872680b0f37aaaa3b5
Sidnei Baron <sidneibaron@gmail.com>
2026-06-02 12:05:20 -0300

    Add installation script for unifebe-so-pp2 service

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install the binary files to /usr/local/bin
+sudo cp ./bin/* /usr/local/bin/
+sudo chmod +x /usr/local/bin/*
+
+# Download the services
+sudo git clone https://github.com/sbaron81/unifebe-so-pp2.git /opt/unifebe-so-pp2
+
+# Inicia servicos
+sudo cp /opt/unifebe-so-pp2/unifebe-so-pp2.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start unifebe-so-pp2 && sudo systemctl enable unifebe-so-pp2 
+
+echo "Instalação concluída com sucesso!"
